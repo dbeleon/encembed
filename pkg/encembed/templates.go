a40502f57c34c88e22644d826ffdb5a079bd734f
dbeleon <dbeleon@yandex.ru>
2024-07-18 08:58:02 +0300

    fix template

--- pkg/encembed/templates.go
+++ pkg/encembed/templates.go
@@ -17,7 +17,7 @@ import(
 var {{.EncryptedVarName}} []byte
 func {{.FuncName}}({{if .ExternalKey}}key string{{end}}) []byte {
 	k := {{if .ExternalKey}}key{{else}}"{{.Key}}"{{end}}
-	{{if .Scramble}}k = descrambleKey(k){{end}}
+	{{if .Scramble}}k = {{.EncryptedVarName}}DescrambleKey(k){{end}}
 	i, _ := age.NewScryptIdentity(k)
 	r, _ := age.Decrypt(bytes.NewReader({{.EncryptedVarName}}), i)
 	a, _ := io.ReadAll(r)
@@ -26,7 +26,7 @@ func {{.FuncName}}({{if .ExternalKey}}key string{{end}}) []byte {
 {{if .DecryptedVarName}}var {{.DecryptedVarName}} = {{.FuncName}}() {{end}}
 
 {{if .Scramble}}
-func descrambleKey(scrKey string) (key string) {
+func {{.EncryptedVarName}}DescrambleKey(scrKey string) (key string) {
 	scram := scr.New({{.ScrLength}}, []int { {{range $v := .ScrFeedbacks}}{{$v}},{{end}} }, uint64({{.ScrPolynomial}}))
 	data, err := base64.StdEncoding.DecodeString(scrKey)
 	if err != nil {
